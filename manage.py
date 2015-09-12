8c3fe03a214ed37ebf671d3dd5567df715bf28e6
al <alwalker57@gmail.com>
2015-09-12 11:11:30 -0400

    Database Creation.

--- manage.py
+++ manage.py
@@ -80,17 +80,19 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-            flash('Better call the Administrator')
-            flash('And close the browser or severe complications may result')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
 
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
-api_manager.create_api(User, methods=['GET', 'POST', 'DELETE', 'PUT'])
+
 
 if __name__ == '__main__':
-    db.create_all()
     manager.run()
